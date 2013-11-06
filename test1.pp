c51c3e02c5f970f24ba9b0814b891190a2000792
Rick Burgess <github@rickburgess.me>
2013-11-06 17:45:19 +0000

    Update test1.pp

--- test1.pp
+++ test1.pp
@@ -2,16 +2,24 @@ node 'client-project-apache'{
    include git
    include supervisor
    
+   exec { "apt-get update":
+       command => "/usr/bin/apt-get update",
+       onlyif => "/bin/sh -c '[ ! -f /var/cache/apt/pkgcache.bin ] || /usr/bin/find /etc/apt/* -cnewer /var/cache/apt/pkgcache.bin | /bin/grep . > /dev/null'",
+   }
+   
    package { "python-pip":
-          ensure => "latest"
+          ensure => "latest",
+          require  => Exec['apt-get update']
    }
    
    package { "python-dev":
-          ensure => "latest"
+          ensure => "latest",
+          require  => Exec['apt-get update']
    }
    
    package { "build-essential ":
-          ensure => "latest"
+          ensure => "latest",
+          require  => Exec['apt-get update']
    }
    
    
@@ -35,11 +43,13 @@ node 'client-project-apache'{
       command   => '/usr/bin/mysqld_safe',
       ensure      => present,
       enable      => true,
+      require  => Package['python-pip']
    }
    
    supervisor::service { 'apache':
       command   => '/etc/apache2/foreground.sh',
       ensure      => present,
       enable      => true,
+      require  => Package['python-pip']
    }
 }
